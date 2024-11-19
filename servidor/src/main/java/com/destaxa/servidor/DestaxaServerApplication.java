bc91a2e32fa75e13377bd77841988a0687b7d0c6
soldiereyes <guilhermegonzagasoares_@hotmail.com>
2024-11-19 17:45:46 -0300

    criação do projeto com SpringBoot

---
+++ servidor/src/main/java/com/destaxa/servidor/DestaxaServerApplication.java
@@ -0,0 +1,13 @@
+package com.destaxa.servidor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DestaxaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DestaxaServerApplication.class, args);
+	}
+
+}
