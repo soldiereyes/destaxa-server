776e68b728c234d3f43e1a8536fed52bad3d256d
soldiereyes <guilhermegonzagasoares_@hotmail.com>
2024-11-21 18:16:21 -0300

    feat(servidor): Controe e interpreta mensagens no formato ISO8583

--- servidor/src/main/java/com/destaxa/servidor/ServerSocketHandler.java
+++ servidor/src/main/java/com/destaxa/servidor/ServerSocketHandler.java
@@ -32,12 +32,29 @@ public class ServerSocketHandler {
 
     private void handleConnection(Socket socket) throws IOException {
         try (socket; BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-             PrintWriter output = new PrintWriter(socket.getOutputStream(), true)) {
+             PrintWriter output = new PrintWriter(socket.getOutputStream(), true)
+        ) {
+            String rawMessage = input.readLine();
+            System.out.println("Message received: " + rawMessage);
 
-            String message = input.readLine();
-            System.out.println("Message received: " + message);
+            ISO8583Message requestMessage = ISO8583Message.parseMessage(rawMessage);
+            ISO8583Message responseMessage = new ISO8583Message("0210");
+            String value = requestMessage.getField(4);
 
-            output.println("Message processed by the server.");
+            if (value == null || Double.parseDouble(value) < 0) {
+                responseMessage.setField(39, "051"); // Negative transaction → Declined
+            } else if (Double.parseDouble(value) > 1000) {
+                System.out.println("Timeout simulated for value > 1000");
+                return;
+            } else {
+                responseMessage.setField(39, "000"); // Approved
+            }
+
+            responseMessage.setField(4, value);
+            output.println(responseMessage.buildMessage());
+            System.out.println("Message sent: " + responseMessage.buildMessage());
+        } finally {
+            socket.close();
         }
     }
 }
