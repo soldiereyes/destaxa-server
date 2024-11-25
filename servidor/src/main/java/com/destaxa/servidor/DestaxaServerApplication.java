f47cc8a6c7454df4080002d610c61f5dbd7c84a5
soldiereyes <guilhermegonzagasoares_@hotmail.com>
2024-11-25 12:14:01 -0300

    feat(servidor): adiciona a thread do socket na main.

--- servidor/src/main/java/com/destaxa/servidor/DestaxaServerApplication.java
+++ servidor/src/main/java/com/destaxa/servidor/DestaxaServerApplication.java
@@ -6,8 +6,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DestaxaServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DestaxaServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DestaxaServerApplication.class, args);
+
+        new Thread(() -> {
+            try {
+                ServerSocketHandler serverSocketHandler = new ServerSocketHandler(5000);
+                serverSocketHandler.start();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
+    }
 
 }
